955761e872795b8e44a55b8d360fedc9da2c0587
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-03-04 00:38:47 -0500

    Update to code

--- index.js
+++ index.js
@@ -6,47 +6,70 @@ const area = pi * radius * radius
 const plantSize = 0.8
 let currentPlantNum = 20
 let week = 0
+const maxArea = area * .8
+
+
+
+// // This part predicts the plant growth after a specified number of weeks at 100% capacity
 
 
 let currentArea = (currentPlantNum * plantSize);
+// console.log(`Week ${week} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
+
+
+// if (currentArea + (currentPlantNum * plantSize) <= area) {
+//     currentPlantNum *= 2;
+//     currentArea = (currentPlantNum * plantSize);
+//         console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
+// } else{
+//     console.log(`Week ${week}: Not enough space for more plants.`);
+// }
+// if (currentArea + (currentPlantNum * plantSize) <= area) {
+//     currentPlantNum *= 2;
+//     currentArea = (currentPlantNum * plantSize);
+//         console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
+// } else {
+//     console.log(`Week ${week += 1}: Not enough space for more plants.`);
+// }
+// if (currentArea + (currentPlantNum * plantSize) <= area) {
+//     currentPlantNum *= 2;
+//     currentArea = (currentPlantNum * plantSize);
+//         console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
+// }
+// else {
+//     console.log(`Week ${week += 1}: Not enough space for more plants.`)
+// }
+
+// Pruned Part. This part sees if the plants shoudl be pruned if the plant count exceeds 80% of the max capacity
+
+
+// 62.830000001 this is 80% max number
+
+
 console.log(`Week ${week} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
 
 
-if (currentArea + (currentPlantNum * plantSize) <= area) {
+if (currentArea + (currentPlantNum * plantSize) <= maxArea) {
     currentPlantNum *= 2;
     currentArea = (currentPlantNum * plantSize);
-        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
-} else{
-    console.log("Week 1: Not enough space for more plants.");
+        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters and shouldn't be pruned`)
+    } 
+    else{
+    console.log(`Week ${week += 1}: The plants have reached 80% capacity and should be pruned.`);
 }
-if (currentArea + (currentPlantNum * plantSize) <= area) {
+if (currentArea + (currentPlantNum * plantSize) <= maxArea) {
     currentPlantNum *= 2;
     currentArea = (currentPlantNum * plantSize);
-        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
+        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters and shouldnt be pruned`)
 } else {
-    console.log("Week 2: Not enough space for more plants.");
+    console.log(`Week ${week += 1}: The plants have reached 80% capacity and should be pruned.`);
 }
-if (currentArea + (currentPlantNum * plantSize) <= area) {
+if (currentArea + (currentPlantNum * plantSize) <= maxArea) {
     currentPlantNum *= 2;
     currentArea = (currentPlantNum * plantSize);
         console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
 }
 else {
-    console.log("Week 3: Not enough space for more plants.")
+    console.log(`Week ${week += 1}: The plants have reached 80% capacity and should be pruned.`)
 }
-if (currentArea + (currentPlantNum * plantSize) <= area) {
-    currentPlantNum *= 2;
-    currentArea = (currentPlantNum * plantSize);
-        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
-}
-else {
-    console.log("Week 4: Not enough space for more plants.")
-}
-if (currentArea + (currentPlantNum * plantSize) <= area) {
-    currentPlantNum *= 2;
-    currentArea = (currentPlantNum * plantSize);
-        console.log(`Week ${week += 1} has ${currentPlantNum} plants and has an area of ${currentArea} Sq. meters`)
-}
-else {
-    console.log("Week 5: Not enough space for more plants.")
-}
+
